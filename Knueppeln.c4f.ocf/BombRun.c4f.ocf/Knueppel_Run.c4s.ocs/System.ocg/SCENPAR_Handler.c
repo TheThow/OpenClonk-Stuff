b8598027409b5e87f2c87f0ebb9d9f14feb0f562
Thow <TheThow@users.noreply.github.com>
2016-02-28 17:26:54 +0100

    Added scenpar for friendly fire to knueppel run

--- Knueppeln.c4f.ocf/BombRun.c4f.ocf/Knueppel_Run.c4s.ocs/System.ocg/SCENPAR_Handler.c
+++ Knueppeln.c4f.ocf/BombRun.c4f.ocf/Knueppel_Run.c4s.ocs/System.ocg/SCENPAR_Handler.c
@@ -7,6 +7,9 @@ global func CheckScenarioParameters()
 		
 	if(SCENPAR_InstaGib == 2)
 		CreateObject(Rule_InstaGib, 0, 0, -1);
+		
+	if(SCENPAR_FriendlyFire == 2)
+		CreateObject(Rule_NoFriendlyFire, 0, 0, -1);
 }
 
 global func ScoreToWin()
