8734b2c9cb12c9db12a0268bcecf4ba9a819e644
Thow <TheThow@users.noreply.github.com>
2016-06-25 16:11:50 +0200

    solid tiles can be built on pillars

--- Clonkraria.ocs/Building.ocd/Libs.ocd/Lib_SolidBuildingTile.ocd/Script.c
+++ Clonkraria.ocs/Building.ocd/Libs.ocd/Lib_SolidBuildingTile.ocd/Script.c
@@ -93,6 +93,9 @@ func BuildingCondition()
 	if (FindObject(Find_Exclude(this), Find_Not(Find_Func("IsPreview")), Find_Func("IsSolidBuildingTile"), Find_Or(Find_OnLine(-GetObjWidth()/2-1, 0, GetObjWidth()/2+1, 0), Find_OnLine(0, -GetObjHeight()/2-1, 0, GetObjHeight()/2+2))))
 		return true;
 	
+	if (FindObject(Find_AtPoint(0, GetObjHeight()), Find_Func("IsPillarBuildingTile")))
+		return true;
+	
 	return false;
 }
 
